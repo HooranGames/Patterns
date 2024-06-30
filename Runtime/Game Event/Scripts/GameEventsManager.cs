a12b22b61e80e24cfc15910a48bafb47e4ab96fe
Amir <amirizadnia2000@gmail.com>
2024-06-30 14:50:02 +0330

    game event system added

---
+++ Runtime/Game Event/Scripts/GameEventsManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Hooran._Packages.Events
+{
+    [DefaultExecutionOrder(-3000)]
+    public partial class GameEventsManager : EventsManagerBase<GameEventsManager>
+    {
+
+    }
+}
